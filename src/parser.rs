546f6e24e71e8ebd3e0bb2f0d33a6132c19b6c53
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2024-09-18 21:29:19 -0400

    Allow any value for lexical categories

--- src/parser.rs
+++ src/parser.rs
@@ -1,37 +1,21 @@
 use nom::bytes::complete::{is_not, tag};
 use nom::character::complete::multispace0;
 use nom::combinator::opt;
-use nom::error::ParseError;
 use nom::sequence::{delimited, pair, Tuple};
 use nom::IResult;
 
-use crate::tree::{LexicalCategory, Node, NodeKind, SyntaxTree};
+use crate::tree::{Node, NodeKind, SyntaxTree};
 
-fn non_space(input: &str) -> IResult<&str, &str> {
-    is_not(" \t\r\n")(input)
-}
-
-fn node_label(input: &str) -> IResult<&str, &str> {
+fn node_content(input: &str) -> IResult<&str, &str> {
     is_not(" \t\r\n[]")(input)
-}
-
-pub fn parse_category(input: &str) -> IResult<&str, LexicalCategory> {
-    let (remaining, result) = non_space(input)?;
-    let category = result.parse().map_err(|_| {
-        nom::Err::Failure(ParseError::from_error_kind(
-            result,
-            nom::error::ErrorKind::Fail,
-        ))
-    })?;
-    Ok((remaining, category))
 }
 
 pub fn parse_node(input: &str) -> IResult<&str, Node> {
     let (remaining, _) = (multispace0, tag("["), multispace0).parse(input)?;
-    let (mut remaining, category) = delimited(multispace0, parse_category, multispace0)(remaining)?;
+    let (mut remaining, category) = delimited(multispace0, node_content, multispace0)(remaining)?;
 
     let kind;
-    if let Ok((r, label)) = node_label(remaining) {
+    if let Ok((r, label)) = node_content(remaining) {
         remaining = r;
         kind = Box::new(NodeKind::Leaf {
             label: label.to_string(),
@@ -46,7 +30,7 @@ pub fn parse_node(input: &str) -> IResult<&str, Node> {
     Ok((
         remaining,
         Node {
-            category,
+            category: category.to_string(),
             kind,
             x: 0.0,
             y: 0.0,
--- src/tree.rs
+++ src/tree.rs
@@ -1,63 +1,3 @@
-use std::fmt::Display;
-use std::str::FromStr;
-
-use anyhow::anyhow;
-
-#[derive(Clone, Copy, Debug)]
-pub enum LexicalCategory {
-    N,
-    NP,
-    V,
-    VP,
-    Det,
-    A,
-    P,
-    PP,
-    C,
-    CP,
-    S,
-}
-
-impl Display for LexicalCategory {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let s = match self {
-            LexicalCategory::N => "N".to_string(),
-            LexicalCategory::NP => "NP".to_string(),
-            LexicalCategory::V => "V".to_string(),
-            LexicalCategory::VP => "VP".to_string(),
-            LexicalCategory::Det => "Det".to_string(),
-            LexicalCategory::A => "A".to_string(),
-            LexicalCategory::P => "P".to_string(),
-            LexicalCategory::PP => "PP".to_string(),
-            LexicalCategory::C => "C".to_string(),
-            LexicalCategory::CP => "CP".to_string(),
-            LexicalCategory::S => "S".to_string(),
-        };
-        write!(f, "{}", s)
-    }
-}
-
-impl FromStr for LexicalCategory {
-    type Err = anyhow::Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "N" => Ok(LexicalCategory::N),
-            "NP" => Ok(LexicalCategory::NP),
-            "V" => Ok(LexicalCategory::V),
-            "VP" => Ok(LexicalCategory::VP),
-            "Det" => Ok(LexicalCategory::Det),
-            "A" => Ok(LexicalCategory::A),
-            "P" => Ok(LexicalCategory::P),
-            "PP" => Ok(LexicalCategory::PP),
-            "C" => Ok(LexicalCategory::C),
-            "CP" => Ok(LexicalCategory::CP),
-            "S" => Ok(LexicalCategory::S),
-            _ => Err(anyhow! {"Invalid lexical category"}),
-        }
-    }
-}
-
 #[derive(Clone, Debug)]
 pub struct SyntaxTree {
     pub root: Node,
@@ -73,7 +13,7 @@ impl SyntaxTree {
 
 #[derive(Clone, Debug)]
 pub struct Node {
-    pub category: LexicalCategory,
+    pub category: String,
     pub kind: Box<NodeKind>,
     pub x: f32,
     pub y: f32,
